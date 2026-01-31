6ebcd16c9da15e85a170dc81433958f30379b5d8
hebe <daiwu527@gmail.com>
2026-01-31 19:02:46 +0800

    feat: add stairs environment and progress reward

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/__init__.py
@@ -5,8 +5,19 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotEnvCfg",
-        "play_env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotPlayEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.stairs_env_cfg:RobotEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.stairs_env_cfg:RobotPlayEnvCfg",
+        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Unitree-G1-29dof-Stairs",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.stairs_env_cfg:RobotStairsEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.stairs_env_cfg:RobotStairsPlayEnvCfg",
         "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
     },
 )
